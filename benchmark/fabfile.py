544a9c0d2a0a0f026993d7f8ba5d0d07cd452931
Ubuntu <ubuntu@ip-172-31-11-68.eu-central-1.compute.internal>
2023-11-09 12:24:05 +0000

    change node params, update ami

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -109,7 +109,7 @@ def remote(ctx, debug=False):
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 10_000,  # ms
         'sync_retry_nodes': 3,  # number of nodes
-        'batch_size': 500_000,  # bytes
+        'batch_size': 50_000,  # bytes
         'max_batch_delay': 200  # ms
     }
     try:
