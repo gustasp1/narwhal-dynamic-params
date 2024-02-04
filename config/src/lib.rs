41ae697c91a93e4d8ca116da28e0bc7e0cd833b9
gustasp1 <gpladis@gmail.com>
2024-02-04 21:56:38 +0000

    .

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -177,7 +177,7 @@ impl Committee {
         // If N = 3f + 1 + k (0 <= k < 3)
         // then (2 N + 3) / 3 = 2f + 1 + (2k + 2)/3 = 2f + 1 + k = N - f
         let total_votes: Stake = self.authorities.values().map(|x| x.stake).sum();
-        (total_votes + 2) / 3
+        2 * total_votes / 3 + 1
         // return match level {
         //     0 => (total_votes + 2) / 3,
         //     1 => 2 * total_votes / 3 + 1,
